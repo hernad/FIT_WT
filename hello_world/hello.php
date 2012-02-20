47fbb0e664bae236f8816c18c12bffdfa5dbb575
Ernad Husremovic <hernad@bring.out.ba>
2012-02-20 18:23:55 +0100

    upd 2

--- hello_world/hello.php
+++ hello_world/hello.php
@@ -12,10 +12,37 @@
 $name = "world";
 
 
-for ($i = 0; $i < 100; $i++) {
+for ($i = 0; $i < 2; $i++) {
    print("Heljo, $name ($i) \n<p>");
 }
 
+$a = array("a"=>"Dog","b"=>"Cat","c"=>"Horse");
+
+$a["a"] = "Pašće" . " grr....";
+echo "<br/>";
+
+print_r($a);
+echo "<br/>";
+
+$tmp = $a["a"];
+
+echo "tmp = $tmp<br/>";
+
+$tmp = date(DATE_ATOM, mktime(12, 30,0, 1, 1, 1970));
+
+echo "tmp = $tmp<br/>";
+
+echo(date(DATE_RFC822) . "<br />");
+
+
+
+$people = array("hernad", "bakir", "samra");
+reset($people);
+
+while (list($key, $val) = each($people))
+{
+	echo "$key => $val<br />";
+}
 
 ?>
 
