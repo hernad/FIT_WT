70cecba258223815a401a008e9dd26deb4c4b45f
Ernad Husremovic <hernad@bring.out.ba>
2012-06-26 21:58:33 +0200

    relatively-absolute positioning

---
+++ hello_world/css.php
@@ -0,0 +1,77 @@
+<html>
+
+<head>
+
+<meta charset="utf-8"></meta>
+
+</head>
+
+<style type="text/css">
+
+ul#my_ul {
+  position: relative;
+  width: 400px; 
+  height: 60px;
+  border: medium solid maroon;
+  border-radius: 2%;
+  padding: 15px;
+  margin: 30px;
+  
+}
+
+li#prvi {
+  position: absolute;
+  margin: 5px;
+  left: 10px;
+  padding-top: 3px;
+  padding-left: 3px;
+  width: 200px;
+  height: 28px;
+  border: thin solid black;
+}
+
+li#drugi {
+  margin: 5px;
+  position: absolute;
+  padding-top: 3px;
+  padding-left: 3px;
+  left: 220px;
+  width: 60px;
+  height: 28px;
+  border: thin solid red;
+}
+
+li#treci {
+  position: absolute;
+  margin: 5px;
+  padding-top: 3px;
+  padding-left: 3px;
+  left: 290px;
+  width: 50px;
+  border: thin solid green;
+  height: 28px;
+}
+li {
+list-style-type: none;
+}
+
+
+
+</style>
+
+<body>
+
+
+</body>
+
+<p>bla bla bla bal vajdklsfad sčklfadjs lčfadjs fčadsjf čadslfj adlsčfjads fčadsj fčldsfj čalskdfj adsčlfj asdčfjasd fčladsj fasčdfj asdfj asdfčdjsfa</p>
+<ul id="my_ul">
+<li id="prvi">jedan: <input type="text" id="txt_1" style="width: 150px;"></input>
+</li>
+<li id="drugi">dva</li>
+<li id="treci">tri</li>
+</ul>
+<p>kraj bla bla bal vajdklsfad sčklfadjs lčfadjs fčadsjf čadslfj adlsčfjads fčadsj fčldsfj čalskdfj adsčlfj asdčfjasd fčladsj fasčdfj asdfj asdfčdjsfa</p>
+
+
+</html>
