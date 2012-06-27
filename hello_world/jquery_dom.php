7e0ae8444da63bf84cac3d5eb57a5361ad61aeb6
Ernad Husremovic <hernad@bring.out.ba>
2012-06-27 07:47:55 +0200

    kako provjeriti radio li checkbox u javascript: input[name=g1]:checked

--- hello_world/jquery_dom.php
+++ hello_world/jquery_dom.php
@@ -27,21 +27,50 @@
   <div></div>
   <div></div>
   <div></div>
+
+  <p/>
+  
+  <br/>
+  
+  <input type="checkbox" name="g1" id="chk1" value="check 1">Check 1</input>
+  <input type="checkbox" name="g1" id="chk2" value="check 2">Check 2</input>
+  <input type="checkbox" name="g1" id="chk3" value="check 3">Check 3</input>
+ 
+ 
+  <br/>
+  
+  <input type="radio" name="g2" id="rb1" value="rb 1">Radio button 1</input>
+  <input type="radio" name="g2" id="rb2" value="rb 2">Radio button 2</input>
+  <input type="radio" name="g2" id="rb3" value="rb 3">Radio button 3</input>
+  
+  <br/>
+  <input type="button" onclick="reci()" value="reci šta je checkirano" ></input>
+  <br/>
+  <input type="button" onclick="reci_radio()" value="reci koji radio je checkiran" ></input>
+  
   
 <script>
   var first_strong = $("strong").slice(2,3); 
   $("p").append( first_strong );
 
-  //$("p#p0").append( '<p id="new">new paragraph</p>' );
-  //$("p#p0").add( '<p id="new">new paragraph</p>' );
-
 
   $("div").css("border", "2px solid red").append("<p>hello</p>").css("background", "yellow");
-  
-  //.add("p")
-  //.css("background", "yellow");
 
+  function reci() {
 
+    $("input[name=g1]:checked").each( function() {
+    alert($(this).val());
+    });
+
+  }
+
+  function reci_radio() {
+
+	    $("input[name=g2]:checked").each( function() {
+	    alert($(this).val());
+	    });
+
+  }
 </script>
 
   
