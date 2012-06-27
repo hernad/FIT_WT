e6bf124fe8e85c48f922aa88e55987b980360eeb
Ernad Husremovic <hernad@bring.out.ba>
2012-06-27 07:16:37 +0200

    .append, .add ... ne kontam ovaj add ?

---
+++ hello_world/jquery_dom.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <style>
+  p { background:yellow; }
+</style>
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+</head>
+
+<style>
+ div { width:60px; height:60px; margin:10px; float:left; }
+ p { clear:left; font-weight:bold; font-size:16px; 
+     color:green; background-color: white; margin:0 10px; padding:2px; }
+</style>
+ 
+
+
+<body>
+  početak <strong>prvi strong</strong>
+  <strong>Hello strong!!!</strong>
+  <p id="p0" >I would like to say: </p>
+
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  
+<script>
+  //var first_strong = $("strong").first(); 
+  //$("p").append( first_strong );
+  //$("p#p0").append( '<p id="new">new paragraph</p>' );
+  $("p#p0").add( '<p id="new">new paragraph</p>' );
+
+
+  $("div").css("border", "2px solid red").append("<p>hello</p>").css("background", "yellow");
+  
+  //.add("p")
+  //.css("background", "yellow");
+
+
+</script>
+
+  
+</script>
+
+</body>
+</html>
