a9e74ec54db7c25b49620cbe9c5c3fe9b08514b4
Ernad Husremovic <hernad@bring.out.ba>
2012-10-02 20:31:18 +0200

    facebook login

--- 09_2012_3/header.php
+++ 09_2012_3/header.php
@@ -4,7 +4,7 @@
    
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
      
-    <link href="templates/public/main.css" rel="stylesheet" type="text/css" />
+    <link href="templates/public/m3.css" rel="stylesheet" type="text/css" />
 
     <link href="scripts/jquery/themes/ui-lightness/jquery-ui-1.8.23.custom.css" rel="stylesheet" type="text/css" />
     <script src="scripts/jquery/jquery-1.8.2.min.js" type="text/javascript"></script>
@@ -14,6 +14,6 @@
     <script src="scripts/jquery/validation/languages/jquery.validationEngine-en.js" type="text/javascript"></script>
     <script src="scripts/jquery/validation/jquery.validationEngine.js" type="text/javascript"></script>
     
-    <script src="utils.js" type="text/javascript"></script>
+    <script src="utils.js"></script>
 
 
---
+++ 09_2012_3/login_3.php
@@ -0,0 +1,52 @@
+<html>
+
+<header>
+
+<?php
+$header = "Login 3";
+include "header.php";
+
+?>
+
+</header>
+
+
+<body>
+
+<div class="box_login">
+
+   <h2>Facebook login:</h2>
+ 
+   <div class="box_login_sub">
+   
+      <div class="form_row">
+         <label>Label 1</label>
+         <input type="text" value="" />
+      </div>
+      
+      <div class="form_row">
+         <label>Label 2</label>
+         <input type="text" value="" />
+      </div>
+     
+      <div class="form_buttons">
+         <input type="submit" value="Log In" />
+          or <a href="test.php">Sigunp for Facebook</a>
+      </div>
+      
+      <div class="form_link_bottom">
+         <a href="test2.php">Forget your pwd ?</a>
+      </div>
+      
+      
+       
+   </div>
+
+
+</div>
+  
+
+</body>
+
+</html>
+
